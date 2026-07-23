64aa0ceaca7e0e66e55667eae684c3a31c279f9b
Marcin Bator <marcinbator.ofc@gmail.com>
2026-07-23 09:53:08 +0200

    chore: sdk bump

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ val isKeystoreConfigured = keystorePropertiesFile.exists()
 
 android {
     namespace = "pl.bator.tatry_kamera"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -35,7 +35,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
