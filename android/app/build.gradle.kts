839479722bd5f5ec1f7e3761c735fbbb414f5e00
Marcin Bator <marcinbator.ofc@gmail.com>
2026-07-23 22:41:52 +0200

    fix sdk v

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         applicationId = "pl.bator.tatry_kamera"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
