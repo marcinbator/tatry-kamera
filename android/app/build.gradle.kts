d2b522a6a0121870cf191ae9090715e3c0cbf03d
Marcin Bator <marcinbator.ofc@gmail.com>
2026-07-23 23:26:59 +0200

    rebuild: flutter package

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ val isKeystoreConfigured = keystorePropertiesFile.exists()
 android {
     namespace = "pl.bator.tatry_kamera"
     compileSdk = 36
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -34,7 +34,11 @@ android {
         applicationId = "pl.bator.tatry_kamera"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 24
+        minSdk = if (project.hasProperty("FLUTTER_MIN_SDK_VERSION")) {
+            (project.findProperty("FLUTTER_MIN_SDK_VERSION") as String).toInt()
+        } else {
+            flutter.minSdkVersion
+        }
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
